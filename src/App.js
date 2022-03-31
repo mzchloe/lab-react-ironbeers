26c008faaee520fc1de4b2880e220d009620cdb6
Jeanet Lin <linett@gmail.com>
2022-03-31 15:32:50 +0200

    iteration 2 - navbar added to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import {Home} from './pages/Home/Home';
 import {Routes, Route} from 'react-router-dom';
+
+//Pages
+import {Home} from './pages/Home/Home';
+import {Navbar} from './components/Navbar';
+import {ListBeers} from './pages/ListBeers/ListBeers';
 
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
      <Routes>
        <Route path="/" element={<Home />}/>
+       <Route path="/all-beer" element={<ListBeers />}/>
      </Routes>
     </div>
   );
