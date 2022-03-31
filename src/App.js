2f0ed7d4f60577041932876a188c852cb97f63ac
Jeanet Lin <linett@gmail.com>
2022-03-31 14:46:09 +0200

    iteration 1 - app.js complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {Home} from './pages/Home';
+import {Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-     <Home />
+     <Routes>
+       <Route path="/" element={<Home />}/>
+     </Routes>
     </div>
   );
 }
